90de3709cf15fd6f065062eedf695565aaff3dda
hardiepienaar <hardiepienaar@gmail.com>
2016-12-12 21:07:08 +0000

    Started work on UART messaging
    Signed-off-by: hardiepienaar <hardiepienaar@gmail.com>

--- code/NVSLib.py
+++ code/NVSLib.py
@@ -5,6 +5,7 @@ H Pienaar Dec 2016
 
 import CHIP_IO.GPIO as GPIO
 import time
+import serial
 
 #Pin defenitions
 PIN_GPIO3 = "XIO-P4"
@@ -12,6 +13,10 @@ PIN_GPIO4 = "XIO-P5"
 PIN_GPIO5 = "XIO-P7"
 PIN_RST = "CSID1"
 PIN_SLP = "CSID0"
+SERIAL_PORT = "/dev/ttyS0"
+
+def soft_reset():
+    ser.write(bytearray([0x10,0x01,0x00,0x01,0x21,0x01,0x00,0x00,0x10,0x03]))
 
 print("Setting up NVS GPIO connections..."),
 #Setup GPIO pins
@@ -23,9 +28,12 @@ GPIO.setup(PIN_SLP, GPIO.IN)
 
 #Initialize output pins
 GPIO.output(PIN_GPIO3, GPIO.LOW)
-GPIO.output(PIN_GPIO4, GPIO.HIGH)
-GPIO.output(PIN_GPIO5, GPIO.HIGH)
+GPIO.output(PIN_GPIO4, GPIO.LOW)
+GPIO.output(PIN_GPIO5, GPIO.LOW)
 GPIO.output(PIN_RST, GPIO.LOW)
+
+#Initialize uart port
+ser = serial.Serial(SERIAL_PORT, 115200, parity=serial.PARITY_ODD)
 print("[DONE]")
 
 time.sleep(0.1)
@@ -35,5 +43,11 @@ GPIO.output(PIN_RST, GPIO.HIGH)
 time.sleep(0.5)
 print("[DONE]")
 
+print("Sending NVS port commands..."),
+#ser.write(bytearray([0x10,0x0B,0x00,0x00,0xC2,0x01,0x00,0x02,0x10,0x03])) #Turn on BINR
+ser.write(bytearray([0x10,0x0B,0x00,0x00,0xC2,0x01,0x00,0x03,0x10,0x03])) #Turn on RTCM 
+print("[DONE]")
+
+
 while True:
-    time.sleep(1)
+    print(ser.readline()) 
