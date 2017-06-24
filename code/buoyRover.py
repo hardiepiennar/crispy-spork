2f7668223f3a9d528ca6495116b4e3d2cd9a40c3
hardie <hardiepienaar@gmail.com>
2017-06-24 11:16:37 +0000

    Progress with setting up radio proxy
    Signed-off-by: hardie <hardiepienaar@gmail.com>

---
+++ code/buoyRover.py
@@ -0,0 +1,8 @@
+"""
+Code for the the rover
+
+Hardie Pienaar
+Makespace Cambridge
+June 2017
+"""
+
--- code/radio_proxy.py
+++ code/radio_proxy.py
@@ -1,7 +1,19 @@
-import os, pty
-import serial.tools.list_ports
-import subprocess
+"""
+This program creates a virtual serial port and forwards the data from the RFM22B radio to the 
+created serial port.
 
+Hardie Pienaar
+Makespace Cambridge
+June 2017
+"""
+
+import os
+import subprocess
+import serial
+import serial.tools.list_ports
+import EZLink_receive as rfm
+
+# Setting up the virtual serial port 
 print("Checking if virtual serial port has been setup")
 ports = os.listdir("/dev/pts/")
 
@@ -12,3 +24,17 @@ if len(ports) < 5:
 else:
 	print("Virtual ports already up and running")
 
+# Open virtual port
+print("Opening virtual port")
+ser = serial.Serial("/dev/pts/2",115200,parity=serial.PARITY_ODD,timeout=None)
+
+# Create RFM22B receive link object
+print("Setting up RFM22B")
+rfm.setup()
+
+# Start forwarding loop
+print("Creating endless posting loop")
+#while True:
+#	rfm.receive_bytes()
+ser.close()
+rfm.close()
